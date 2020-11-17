4afc68a3bc689235def1f8cd36f806b2665e73d2
tylerfierro <tmfierro@outlook.com>
2020-11-17 11:16:22 -0800

    Commit 2

--- LMSFinals.UI.MVC/Startup.cs
+++ LMSFinals.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(LMSFinals.UI.MVC.Startup))]
 namespace LMSFinals.UI.MVC
 {
     public partial class Startup
